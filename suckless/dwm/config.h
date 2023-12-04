3a60be22084821ac417e77054d91ea67e40fc9a9
david-star-git <121257683+david-star-git@users.noreply.github.com>
2023-12-04 11:06:39 -0300

    Update config.h

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -30,7 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "librewolf",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "brave",    NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -62,15 +62,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *librewolf[] = { "librewolf", NULL };
+static const char *bravecmd[] = { "brave", NULL };
 
 #include "exitdwm.c"
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_f,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,	                      XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY,                       XK_b,      spawn,          {.v = librewolf } },
+	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
+  	{ MODKEY,                       XK_b,      spawn,          {.v = bravecmd } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
